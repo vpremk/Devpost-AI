cede05fc0c2c3a9758561f134e7d3e0420ea7d97
Vandana Premkumar <5053940+vpremk@users.noreply.github.com>
2026-04-01 12:27:20 -0400

    chore: fixing playwrite

--- playwright.config.js
+++ playwright.config.js
@@ -26,7 +26,7 @@ export default defineConfig({
   },
 
   webServer: {
-    command: 'npm start',
+    command: 'npm run dev',
     url: 'http://localhost:3000',
     reuseExistingServer: !process.env.CI,
     timeout: 120 * 1000,
